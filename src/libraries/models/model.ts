afff48137876d4e83f02cf295872e11bcfded4e6
Danny Vaca <danny270793@icloud.com>
2025-10-24 15:08:31 -0500

    feat: implement Model.create() method with SQLite persistence

--- src/libraries/models/model.ts
+++ src/libraries/models/model.ts
@@ -1,19 +1,56 @@
 import { Column } from "./column";
+import Insert from "../basics/insert";
+import DatabaseManager from "../database";
 
 export abstract class Model {
+    protected static getTableName(): string {
+        // Convert class name to snake_case table name
+        // e.g., User -> user, UserProfile -> user_profile
+        return this.name
+            .replaceAll(/([A-Z])/g, (match, p1, offset) => 
+                offset > 0 ? `_${p1.toLowerCase()}` : p1.toLowerCase()
+            )
+            .toLowerCase();
+    }
+
     static async create<T extends Model>(this: new () => T, data: Record<string, any>): Promise<T> {
-        // TODO: Create data in database
         const instance = new this();
+        const tableName = (this as any).getTableName();
         
+        // Set column values in the instance
         for (const key in data) {
             if ((instance as any)[key] instanceof Column) {
                 (instance as any)[key].set(data[key]);
             }
         }
         
+        // Prepare data for insertion
+        const insertData: Record<string, any> = {};
+        for (const key in instance) {
+            if ((instance as any)[key] instanceof Column) {
+                const column = (instance as any)[key] as Column;
+                insertData[column.getName()] = column.get();
+            }
+        }
+        
+        // Create INSERT statement and execute
+        const insertQuery = Insert.into(tableName).rows(insertData);
+        const statement = insertQuery.toSQL();
+        
+        const db = DatabaseManager.getInstance();
+        const result = db.execute(statement);
+        
+        // If no ID was provided, use the lastInsertRowid from the database
+        if (!data.id && result.lastInsertRowid) {
+            const idColumn = (instance as any).id;
+            if (idColumn instanceof Column) {
+                idColumn.set(String(result.lastInsertRowid));
+            }
+        }
+        
         return instance;
     }
-
+    
     static async find<T extends Model>(this: new () => T, id: string): Promise<T> {
         // TODO: Get data from database
         const data: Record<string, any> = { id };
