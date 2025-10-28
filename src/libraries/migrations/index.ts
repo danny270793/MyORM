d364800766ae04f128912e9b3660778138f8121b
Danny Vaca <danny270793@icloud.com>
2025-10-28 13:52:31 -0500

    feat: add Migrations helper class for migration setup orchestration

---
+++ src/libraries/migrations/index.ts
@@ -0,0 +1,23 @@
+import { migrations } from "../../migrations";
+import { Logger } from "../logger";
+import { Schema } from "../schema";
+
+const logger: Logger = new Logger('./src/libraries/migrations/index.ts');
+
+export class Migrations {
+    static async setup(): Promise<void> {
+        logger.debug('migrations to apply', migrations.length);
+        migrations.sort((a, b) => a.getMigrationNumber() - b.getMigrationNumber());
+
+        const schema: Schema = new Schema();
+        for (const migration of migrations) {
+            if(await migration.wasApplied(schema)) {
+                logger.debug('migration already applied', migration.getMigrationNumber());
+                continue
+            }
+
+            logger.debug('applying migration', migration.constructor.name);
+            migration.up(schema);
+        }
+    }
+}
