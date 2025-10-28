89c8a17dbae2effedc5df38e479bb4468ec168fc
Danny Vaca <danny270793@icloud.com>
2025-10-28 13:58:14 -0500

    style: format migration.ts

--- src/libraries/migrations/migration.ts
+++ src/libraries/migrations/migration.ts
@@ -1,16 +1,19 @@
-import { Schema } from "../schema";
+import { Schema } from '../schema';
 
 export abstract class Migration {
     abstract getMigrationNumber(): number;
     abstract up(schema: Schema): void;
     abstract down(schema: Schema): void;
-    
+
     async wasApplied(schema: Schema): Promise<boolean> {
         return schema.isMigrationApplied(this.getMigrationNumber());
     }
-    
+
     async apply(schema: Schema): Promise<void> {
         this.up(schema);
-        schema.recordMigration(this.getMigrationNumber(), this.constructor.name);
+        schema.recordMigration(
+            this.getMigrationNumber(),
+            this.constructor.name,
+        );
     }
 }
